accaed1d90a61642626f6654d8e720940ff8abe0
Karot22 <mariebabkine@gmail.com>
2026-05-07 05:04:48 +0200

    maj alignement

--- php/dual.php
+++ php/dual.php
@@ -11,11 +11,11 @@
     <div class="split">
         <div class="panel left">
             <h2>Vivez l'interactif</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu egestas libero. Praesent at mattis enim, nec auctor purus. In tempus nulla sed turpis congue iaculis. Suspendisse potenti. Suspendisse pellentesque.</p>
+            <p>Une expérience riche en documents, en témoignages, pensée pour approfondir votre connaissance des chaebols et de leurs histoires tout en choisissant votre chemin parmi les documents. </p>
         </div>
         <div class="panel right">
             <h2>Regarder le documentaire</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu egestas libero. Praesent at mattis enim, nec auctor purus. In tempus nulla sed turpis congue iaculis. Suspendisse potenti. Suspendisse pellentesque.</p>
+            <p>Deux étudiantes qui relèvent une défi en traversant la ville de Busan, en Corée du Sud.  <br> Pourront elles arriver à destination en évitant les service et les produits des chaebols ?</p>
         </div>
     </div>
 
